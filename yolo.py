72743821eefb57e9b2f357e944c81a22fb8259be
Ricardo Marin <ricardo_k_marin@hotmail.com>
2025-06-23 23:56:52 -0300

    RUN_GPU_V2
    Modelo treinado pelo YOLO12N através da GPU, diferença do tempo de treinamento de 1 hora via CPU para 5 minutos via GPU.

--- yolo.py
+++ yolo.py
@@ -3,7 +3,15 @@ ultralytics.checks()
 
 from ultralytics import YOLO
 
-model = YOLO('yolo12n.pt')
+# A função principal que encapsula o código de execução
+def run_yolo_training():
+    ultralytics.checks() # É bom manter essa checagem
+
+    model = YOLO('yolo12n.pt')
+
+    # Se você estiver usando um DataLoader com num_workers > 0 (o que é o padrão no Ultralytics para GPUs),
+    # essa parte precisa estar protegida pelo if __name__ == '__main__'.
+    results = model.train(data="Trabalho_G2_CG.yolov12/data.yaml", epochs=100, imgsz=640, plots=True)
 
 
 results = model.train(data="Trabalho_G2_CG.yolov12\data.yaml", epochs=100, imgsz=640, plots=True)
